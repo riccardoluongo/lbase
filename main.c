591f3d6b9c20d096e9897f97225817b00a7b684e
riccardoluongo <ildio4nubi@gmail.com>
2025-08-31 19:59:45 +0200

    - added tombstones - now using fscanf for reads

--- main.c
+++ main.c
@@ -35,37 +35,35 @@ int main(int argc, char ** argv){
             exit(1);
         };
 
+        fprintf(new, "L\n");
+
         printf("file %s created successfully\n", argv[1]);
         exit(0);
     }
-    if(argc < 4 || (argc < 6 && strcmp(argv[2], "-r") != 0)){
+    if(argc < 4 || (argc < 5 && (strcmp(argv[2], "-r") != 0 && strcmp(argv[2], "-d") != 0))){
         printf("error: target element not specified\n");
         return -1;
-    } else if((strcmp(argv[2], "-w") == 0 && argc > 6) || (strcmp(argv[2], "-r") == 0 && argc > 4)){
+    } else if((strcmp(argv[2], "-w") == 0 && argc > 5) || ((strcmp(argv[2], "-r") == 0 || strcmp(argv[2], "-d") == 0) && argc > 4)){
         printf("error: too many arguments\n");
         return -1;
     }
     for(uint8_t i = 1; i < argc; i++){
         if(strcmp(argv[i], "-h") == 0){
-            printf("command format: lbase [filename] [mode [-w, -r]] [type [i, s, b]] [key] [value]\n");
+            printf("command format: lbase [filename] [mode [-w, -r]] [key] [value]\n");
         }
     }
 
-    if(
-        argv[2][0] != '-' ||
-        (argv[2][1] != 'r' && argv[2][1] != 'w') ||
-        (argv[2][1] == 'w' && (strlen(argv[3]) != 1 || (argv[3][0] != 'i' && argv[3][0] != 's' && argv[3][0] != 'b') || strlen(argv[4]) > 31))
-    ){
+    if(argv[2][0] != '-' || (argv[2][1] != 'r' && argv[2][1] != 'w' && argv[2][1] != 'd') || (argv[2][1] == 'w' && strlen(argv[4]) > 31)){
         printf("error: invalid argument format, run with -h for help\n");
         return -1;
     }
 
-    if(strcmp(argv[2], "-w") == 0){
-        db = fopen(argv[1], "ab");
-        fprintf(db, "%s %c %s\n", argv[4], argv[3][0], argv[5]);
-    }
+    if(strcmp(argv[2], "-w") == 0)
+        fprintf(fopen(argv[1], "ab"), "r:%s %s\n", argv[3], argv[4]);
     else if(strcmp(argv[2], "-r") == 0){
-        char buf[32], c;
+        char c;
+        char key[32], val[1024], record_type[3];
+        int8_t fscanf_status;
         unsigned long prev_record_end;
 
         db = fopen(argv[1], "rb");
@@ -75,24 +73,37 @@ int main(int argc, char ** argv){
             if((c = fgetc(db)) != '\n')
                 fseek(db, -2, SEEK_CUR); //find start of the record
             else{
-                uint8_t bufpos = 0;
                 prev_record_end = ftell(db) - 2;
 
-                while((c = fgetc(db)) != ' ') //store key
-                    buf[bufpos++] = c;
-                buf[bufpos] = '\0';
+                if((fscanf_status = fscanf(db, "%2s", record_type))){
 
-                if(strcmp(buf, argv[3]) == 0){ //key matches
-                    fseek(db, 2, SEEK_CUR); //skip value type
-                    while(putchar(fgetc(db)) != '\n') //print requested value
-                        ;
-                    break;
+                    if(record_type[0] == 'r'){ //is regular record
+                        if((fscanf_status = fscanf(db, "%31s %1023s", key, val) == 2)){
+                            if(strcmp(key, argv[3]) == 0){
+                                printf("%s\n", val);
+                                break;
+                            }
+                            fseek(db, prev_record_end, SEEK_SET);
+                        }
+                        else if(fscanf_status == EOF) goto eof;
+                        else goto corrupt_err;
+                    }
+                    else if(record_type[0] == 't') break;//is tombstone
+                    else goto corrupt_err;
                 }
-
-                fseek(db, prev_record_end, SEEK_SET); //skip already processed record
+                else if(fscanf_status == EOF) goto eof;
+                else goto corrupt_err;
             }
         }
-    }
+    } else if(strcmp(argv[2], "-d") == 0)
+        fprintf(fopen(argv[1], "ab"), "t:%s\n", argv[3]);
 
     exit(0);
+
+    corrupt_err:
+        printf("error: data is corrupted\n");
+        exit(1);
+    eof:
+        printf("error: fscanf() returned EOF\n");
+        exit(1);
 }
